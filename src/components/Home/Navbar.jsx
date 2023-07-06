d31eadf62d2a496605c210e6776bf1a7a437820b
McRonaah <ronaldcheruiyot342@gmail.com>
2023-07-07 01:27:44 +0300

    :see_no_evil: created navbar

--- src/components/Home/Navbar.jsx
+++ src/components/Home/Navbar.jsx
@@ -1,22 +1,37 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import "./CSS/navbar.css"
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className='navbar'>
       {/* <div className="logo">
         <img src={logo} alt="Logo" />
       </div> */}
       <ul className="nav-links">
         <li>
-          <a href="/">Home</a>
+          <a href="/">HOME</a>
         </li>
         <li>
-          <a href="/about">About</a>
+          <a href="/about">ABOUT US</a>
         </li>
         <li>
-          <a href="/contact">Contact</a>
+          <a href="/plans">PLANS</a>
         </li>
+        <li>
+          <a href="/faqs">FAQs</a>
+        </li>
+        <li>
+          <a href="/support">SUPPORT</a>
+        </li>
+        <div>
+        <li>
+          <a href="/signup">SIGN UP</a>
+        </li>
+        <li>
+          <a href="/login">LOGIN</a>
+        </li>
+        </div>
       </ul>
     </nav>
   )
