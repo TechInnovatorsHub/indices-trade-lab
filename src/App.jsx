724c392c6bf3adfcd0899bd6aa4f4bb3a49aefbf
McRonaah <ronaldcheruiyot342@gmail.com>
2023-07-07 01:27:16 +0300

    :clown_face: fixes proper routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Navbar  from "./components/Home/Navbar";
+import { AnimatePresence } from 'framer-motion';
 import Landingpage from "./components/Home/Landingpage";
 
 function App() {
   return (
-    <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landingpage />} />
-        </Routes>
-
-    </BrowserRouter>
+    <AnimatePresence wait>
+      <Navbar />
+    <div className="w-screen h-auto flex  flex-col" >
+      {/* <Navbar /> */}
+        <main className="mt-14 md:mt-20 px-4 md:px-16 py-4 w-full">
+          <Routes>
+            <Route path="/" element={<Landingpage />} />
+          </Routes>
+        </main>
+        
+    </div>
+    </AnimatePresence>
   );
 }
 
