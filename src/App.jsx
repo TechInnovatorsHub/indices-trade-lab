5e99a406b4d1a6df991558684be4ae22e854d617
ABEL <abellronoh@gmail.com>
2023-07-09 22:31:50 +0300

    :fire: created works folder

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar  from "./components/Home/Navbar";
 import { AnimatePresence } from 'framer-motion';
-import Landingpage from "./components/Home/Landingpage";
+// import Landingpage from "./components/Home/Landingpage";
+import Works from "./components/Works/Works";
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
       {/* <Navbar /> */}
         <main className="mt-14 md:mt-20 px-4 md:px-16 py-4 w-full">
           <Routes>
-            <Route path="/" element={<Landingpage />} />
+            {/* <Route path="/" element={<Landingpage />} /> */}
+            <Route path="/" element={<Works />} />
           </Routes>
         </main>
         
---
+++ src/components/Works/Works.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Works() {
+    return (
+        <div>Works
+            3IJERKLNJRJHM
+            WEHAFSKJY
+                <p>ensdbqwfek</p>
+        </div>
+    );
+}
+
+export default Works;
