c57df4e63030d83005118b92fa71400bff562ec6
McRonaah <ronaldcheruiyot342@gmail.com>
2023-07-11 14:42:20 +0300

    :bulb: updated started codes :bulb:

---
+++ src/components/AuthPage/Register.jsx
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react"
+
+const Register = () => {
+  return (
+    <div>
+        <form action="#">
+			{/* <h1>Create Account</h1>
+			<span>or use your email for registration</span> */}
+			<input type="text" placeholder="Name" />
+			<input type="email" placeholder="Email" />
+			<input type="password" placeholder="Password" />
+			<button>Sign Up</button>
+		</form>
+    </div>
+  )
+}
+
+export default Register;
---
+++ src/components/Plans/Plans.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const Plans = () => {
+  return (
+    <div>Plans</div>
+  )
+}
+
+export default Plans;
