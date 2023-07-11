1f5a0f102e51f1a7975beeb1813056e915f9c289
KubasuIvanSakwa <kubasucuba2020@gmail.com>
2023-07-11 20:33:49 +0300

    styled the support page in support.css page

--- src/components/Support/Support.jsx
+++ src/components/Support/Support.jsx
@@ -2,6 +2,9 @@ import { NavLink, Link } from "react-router-dom"
 import "./support.css"
 import mail from "./Assets/mail.svg"
 import back from "./Assets/back.svg"
+import phone from "./Assets/phone.svg"
+import email from "./Assets/email1.svg"
+import chat from "./Assets/chat.svg"
 
 function Support() {
     {/*this function will remove the iamActive class from the navbar when it goes back to the homepage*/}
@@ -20,10 +23,13 @@ function Support() {
             </section>
             <section className="support-main">
                 {/*this Link goes back to the homepage but calls the removeIamActiveClass function to remove the iamActive state so that the navbar can be rendered */}
-                <Link to=".." 
-                    onClick={removeIamActiveClass}>
+                <NavLink 
+                    className="back"
+                    to="../" 
+                    onClick={removeIamActiveClass}
+                >
                     <img src={back} />
-                </Link>
+                </NavLink>
 
                 <div className="support-contact">
                     <h3>Contact us:</h3>
@@ -35,47 +41,70 @@ function Support() {
                         You can reach us through the following channels;
                     </p>
                 </div>
-                <hr />
+                <div className="line">
+                    <hr />
+                </div>
+
                 <div className="phone-support">
                     <h3>Phone Support</h3>
-                    <p>pic</p>
-                    <h5>Telephone</h5>
-                    <p>
-                        call us toll-free at 1-800-FOREX.
-                    </p>
-                    <p>
-                        Our support team is available 24/7 to assist you.
-                    </p>
+                    <div className="phone-1">
+                        <img src={phone}/>
+                        <div className="phone-inner">
+                            <h5>Telephone</h5>
+                            <p>
+                                call us toll-free at 1-800-FOREX.
+                            </p>
+                            <p>
+                                Our support team is available 24/7 to assist you.
+                            </p>
+                        </div>
+                    </div>
                 </div>
-                <hr />
+
+                <div className="line">
+                    <hr />
+                </div>
+
                 <div className="online-support">
-                    <p>pic</p>
-                    <h5>EMAIL</h5>
-                    <p>
-                        send us an email at support@forex.com. We
-                        aim to respond to all inquiries within 24 hours
-                    </p>
-                    <p>pic</p>
-                    <h5>LIVE CHAT</h5>
-                    <p>
-                        Click here to chat with a representation in real-time.
-                    </p>
-                    <p>
-                        Our live chat services is available 24/7 for your convenience
-                    </p>
+                    <div className="online-1">
+                        <img src={email}/>
+                        <div className="online-inner">
+                            <h5>EMAIL</h5>
+                            <p>
+                                send us an email at support@forex.com. We
+                                aim to respond to all inquiries within 24 hours
+                            </p>
+                        </div>
+                    </div>
+                    <div className="onlin-2">
+                        <img src={chat}/>
+                        <div className="onlin-inner">
+                            <h5>LIVE CHAT</h5>
+                            <p>
+                                Click here to chat with a representation in real-time.
+                            </p>
+                            <p>
+                                Our live chat services is available 24/7 for your convenience
+                            </p>
+                        </div>
+                    </div>
                 </div>
-                <hr />
-            </section>
+
+                <div className="line">
+                    <hr />
+                </div>
+
             <section className="support-buttons">
-                <Link to="">
+                <Link className="create" to="">
                     <p>Create Account</p>
                     <p>Join us Today</p>
                 </Link>
 
-                <Link to="">
+                <Link className="login" to="">
                     <p>MEMBER LOGIN</p>
                     <p>Welcome back</p>
                 </Link>
+            </section>
             </section>
         </div>
     )
