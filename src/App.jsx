3bdb5078373ed5d5a82b17d70a32e8586f5c0acf
McRonaah <ronaldcheruiyot342@gmail.com>
2023-07-15 00:39:35 +0300

    :sparkles: added more folders

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar  from "./components/Home/Navbar";
 import { AnimatePresence } from 'framer-motion';
-// import Landingpage from "./components/Home/Landingpage";
+import Landingpage from "./components/Home/Landingpage";
 import Works from "./components/Works/Works";
 import About from './components/About/About';
 import Plans from './components/Plans/Plans';
@@ -11,6 +11,12 @@ import FAQs from './components/FAQs/Faqs';
 import Support from './components/Support/Support';
 import Signup from "./components/AuthPage/Signup";
 import Login from './components/AuthPage/Login';
+
+import Withdrawals from "./components/Withdrawals/Withdrawal";
+import Deposits from "./components/Dashboard/Deposit/Deposit";
+import Livetrading from "./components/Dashboard/Livetrading/Livetrading";
+import Connect from "./components/Connects/Connect";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 function App() {
   return (
@@ -20,18 +26,22 @@ function App() {
       {/* <Navbar /> */}
         <main className="mt-14 md:mt-20 px-4 md:px-16 py-4 w-full">
           <Routes>
-            {/* <Route path="/" element={<Landingpage />} />  */}
-            <Route path="/" element={<Works />} />
-            {/* <Route path="/" element={<Landingpage />} /> */}
-            <Route path="/About" element={<About />} />
-            <Route path="/Plans" element={<Plans />} />
-            <Route path="/FAQs" element={<FAQs />} />
+            <Route path="/" element={<Landingpage />} /> 
+            <Route path="/works" element={<Works />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/plans" element={<Plans />} />
+            <Route path="/faqs" element={<FAQs />} />
             <Route path="/support" element={<Support />} />
-            <Route path="/Signup" element={<Signup />} />
-            <Route path="/Login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/withdrawals" element={<Withdrawals />} />
+            <Route path="/deposits" element={<Deposits />} />
+            <Route path="/livetrading" element={<Livetrading />} />
+            <Route path="/connect" element={<Connect />} />
           </Routes>
         </main>
-        
     </div>
     </AnimatePresence>
   );
