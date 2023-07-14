bad5b4ec10776d5943303bb4f10e25acc6595e1c
lenox-isindu <lenox1739@gmail.com>
2023-07-14 17:45:14 +0300

    :white_check_mark: updated plans page :mag:

--- src/components/Plans/Plans.jsx
+++ src/components/Plans/Plans.jsx
@@ -4,6 +4,7 @@ import "./plans.css";
 
 const Plans = () => {
   return (
+    <section className='planspage'>
     <div>
       <section className='plans'> 
         <h1>OUR PLANS</h1>
@@ -11,40 +12,50 @@ const Plans = () => {
 
       <div className="cards-container">
         <div className="card">
-          <h2>BASIC PLAN</h2>
+          <h2><b><i>BASIC PLAN</i><br/>$ 10/month</b></h2>
+          <hr/>
           <p>
-            Access to educational resources and tutorials. <br/>
-            Basic market analysis and daily market updates.<br/>
-            Competitive floating spreads.<br/>
-            Limited number of trading signals..<br/>
-            Standard customer support.
-          </p>
+            <ol>
+            <li>-Access to educational resources and tutorials.</li> 
+          <li>  -Basic market analysis and daily market updates.</li>
+           <li> -Competitive floating spreads.</li>
+           <li> - Limited number of trading signals.</li>
+           <li> -Standard customer support.</li>
+            </ol>
+            </p>
+            <button class="button" ><b>choose plan</b></button>
+          
         </div>
 
         <div className="card middle-card">
-          <h3>ADVANCED PLAN</h3>
+          <h3><b><i>ADVANCED PLAN</i><br/>$ 30/month</b></h3>
+          <hr/>
           <p>
-            Advanced trading courses and expert insights.<br/>
-            Premium market analysis<br/>
-            High-quality trading signals.<br/>
-            Access to proprietary trading tools and algorithms.<br/>
-            24/7 dedicated customer support.<br/>
+            -Advanced trading courses and expert insights.<br/>
+            -Premium market analysis<br/>
+            -High-quality trading signals.<br/>
+           -Access to proprietary trading tools and algorithms.<br/>
+           -24/7 dedicated customer support.<br/>
             One-on-one coaching with experienced traders.
           </p>
+          <button class="button" ><b>choose plan</b></button>
         </div>
 
         <div className="card">
-          <h4>VIP PLAN</h4>
+          <h4><b><i>VIP PLAN</i><br/>$ 50/month</b></h4>
+          <hr/>
           <p>
-            VIP market trading signals from top analysts.<br/>
-            Priority access to cutting-edge trading tools.<br/>
-            Premium customer support with dedicated account manager.<br/>
-            Invitations to exclusive events and seminars.<br/>
-            Regular performance reviews.
+            -VIP market trading signals from top analysts.<br/>
+            -Priority access to cutting-edge trading tools.<br/>
+           -Premium customer support with dedicated account manager.<br/>
+            -Invitations to exclusive events and seminars.<br/>
+            -Regular performance reviews.
           </p>
+          <button class="button" ><b>choose plan</b></button>
         </div>
       </div>
     </div>
+    </section>
   )
 }
 
