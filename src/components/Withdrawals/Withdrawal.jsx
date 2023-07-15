7dd2103462a0352990210d9af59e704b6bd8d494
Serem-manu <serem695@gmail.com>
2023-07-15 10:40:10 +0300

    :mag: imported css file

--- src/components/Withdrawals/Withdrawal.jsx
+++ src/components/Withdrawals/Withdrawal.jsx
@@ -1,9 +1,10 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import "./Withdrawal.css";
 
 const Withdrawal = () => {
   return (
-    <div>Withdrawal</div>
+    <div className='container'>Withdrawal</div>
   )
 }
 
