4fefe6def52b05bc62ac7f6a0d97731ca6a8dfb4
McRonaah <ronaldcheruiyot342@gmail.com>
2023-07-17 15:49:04 +0300

    :stethoscope: addded dumpy data to connects page :thread:

--- src/components/Connects/Connect.jsx
+++ src/components/Connects/Connect.jsx
@@ -7,54 +7,59 @@ import ceo from "../../assets/ceo.jpeg";
 
 const Connect = () => {
   return (
-    <div className="container">
-      <header>
-        <nav className="header-nav">
-          <a href="/login">Login</a>
-        </nav>
-        <button className="cta-button">Trade Now</button>
-      </header>
+    <div className="flex flex-col items-center justify-center bg-gray-200 pb-10">
+    <div className="flex items-center justify-between w-full  mt-4">
+      <button className="ml-2">Register</button>
+      <h1 className="text-2xl font-bold">Forex</h1>
+      <button className="mr-2">Login</button>
+    </div>
 
-      <main>
-        <section className="hero-section">
-          <h1>Welcome to MarketsHub</h1>
-          <p>Explore markets, trade with confidence.</p>
-        </section>
-
-        <section className="markets-section">
-          <h2>Markets We Operate In</h2>
-          <ul>
+    <div className="flex items-center justify-center mt-8">
+      <div className="w-1/2">
+        <div className="flex items-center justify-center mt-8">
+          <div className="flex items-center mb-4">
+            <img
+              className="w-24 h-24 rounded-full"
+              src={cto}
+              alt="Person 1"
+            />
+            <div className="ml-4">
+              <h2 className="text-xl font-bold">Person 1</h2>
+              <p className="text-sm">Brief description about Person 1</p>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <img
+              className="w-24 h-24 rounded-full"
+              src={ceo}
+              alt="Person 2"
+            />
+            <div className="ml-4">
+              <h2 className="text-xl font-bold">Person 2</h2>
+              <p className="text-sm">Brief description about Person 2</p>
+            </div>
+          </div>
+        </div>
+        
+        <div className="mt-8 text-center">
+          <p className="text-lg mb-4">Explore markets, trade with confidence.</p>
+          <h2 className="text-xl font-bold">Markets We Operate In</h2>
+          <ul className="list-disc list-inside mt-2">
             <li>Stocks</li>
             <li>Cryptocurrency</li>
             <li>Forex</li>
           </ul>
-        </section>
+        </div>
+      </div>
 
-        <section className="certificate-section">
-          <div className="certificate-info">
-            <h3>Certificate Sample</h3>
-            <img src={certificate} alt="Certificate Sample" />
-          </div>
-        </section>
-
-        <section className="owners-section">
-          <div className="owner-profile">
-            <img src={ceo} alt="Owner 1" />
-            <h4>John Doe</h4>
-            <p>Co-founder & CEO</p>
-          </div>
-          <div className="owner-profile">
-            <img src={cto} alt="Owner 2" />
-            <h4>Jane Smith</h4>
-            <p>Co-founder & CTO</p>
-          </div>
-        </section>
-      </main>
-
-      <footer>
-        <p>&copy; 2023 MarketsHub. All rights reserved.</p>
-      </footer>
+      <div className="w-1/2 flex flex-col items-center justify-center">
+        <div className="mt-8">
+          <h2 className="text-2xl font-bold mb-4">Certificate</h2>
+          <img className="w-full h-auto" src={certificate} alt="Certificate" />
+        </div>
+      </div>
     </div>
+  </div>
   );
 }
 
