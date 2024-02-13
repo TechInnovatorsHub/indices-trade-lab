fc4a78c16bd6fa6d5e43e6449577bfa7838100ac
KubasuIvanSakwa <kubasuivansakwa23@gmail.com>
2024-02-13 17:42:55 +0300

    updates

--- src/components/Home/Landingpage.jsx
+++ src/components/Home/Landingpage.jsx
@@ -1,19 +1,43 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import bgImage from "./assets/hero-bg.svg"
+import heroImage1 from "./assets/heroImage1.svg"
+import { NavLink } from 'react-router-dom'
+
 
 
 const Landingpage = () => {
+
   return (
     <section 
       className="hero1 h-[90vh] bg-zinc-950 text-xl text-white pt-[7rem] pl-[4rem] after:content[''] 
         after:absolute after:top-0 after:left-0
         after:z-10 after:w-[100%] after:h-[90%] after:opacity-10 
         after:bg-[url('/home/hace01/Desktop/Indices/indices-trade-lab/src/components/Home/assets/hero-bg.svg')]
-        after:bg-no-repeat after:bg-cover"
+        after:bg-no-repeat after:bg-cover overflow-hidden"
       >
-        <section className="slider">
+        <section className="slider flex">
+            <div className="content relative top-[8rem]">
+              <h1 className="text-5xl mb-3 font-bold">
+                New standard <br />in stock broker.
+              </h1>
+              <p className="text-2xl">
+                Trade forex, commodities, synthetic <br /> and 
+                stock indices from a single account
+              </p>
+              <NavLink 
+                    to="/signup" 
+                    className="flex bg-none w-[15rem] hover:bg-[#FC5B3F] hover:text-white text-[#FC5B3F]
+                      text-lg border border-[#FC5B3F] rounded-lg p-[0.5rem] 
+                      justify-center items-center h-[3rem] relative top-4 z-20"
+                  >
+                    Get Started
+              </NavLink>
+            </div>
 
+            <div className="image">
+              <img src={heroImage1} className="w-[50rem] relative right-[-10%] bottom-[5%] z-20"/>
+            </div>
         </section>
     </section>
 
--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -1,77 +1,10 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react';
-import "./works.css";
-import a from '../../assets/images/works_image/1.png'
-import b from '../../assets/images/works_image/2.png'
-import c from '../../assets/images/works_image/3.png'
 
 function Works() {
     return (
-        <div className='main-works'>
-            <button className='works_trade'>Trade Now</button>
-           <div className='heading'>
-            <h1>How it Works</h1>
-           </div>
-           <div className='Hcards'>
-                <div className="cards">
-                    <h1>Deposit</h1>
-                    <img src= {a}/>
-                    <p>Open real account and add funds. We work with more than 20 payment systems.</p>
-                </div>
-                <div className="cards">
-                    <h1>Trade</h1>
-                    <img src= {b}/>
-                    <p>Trade any of 100 assets and stocks. Use technical analysis and trade the news</p>
-                </div>
-                <div className="cards">
-                    <h1>Withdraw</h1>
-                    <img src= {c}/>
-                    <p>Get funds easily to your bank card or e-wallet. We take no commission.</p>
-                </div>
-
-            </div>
-            <div className='signals'>
-                    <div className='currency1'>
-                    <div className='Cname'>
-                        <div className='flag'>
-                        <svg>
-                        </svg>
-                            <h4>USD/JPY <br/> USD/JPY </h4>
-                        </div>
-                        <p className='rise'>+0.24</p>
-                        <div>
-                            <button className='sell'>Sell</button>
-                            <button className='buy'>Buy</button>
-                        </div>
-                    </div>
-                    <div className='Cname'>
-                    <div className='flag'>
-                        <svg>
-                        </svg>
-                            <h4>USD/JPY <br/> USD/JPY </h4>
-                            </div>
-                            <p className='fall'>-0.84</p>
-                            <div>
-                            <button className='sell'>Sell</button>
-                            <button className='buy'>Buy</button>
-                        </div>
-                    </div>
-                    <div className='Cname'>
-                    <div className='flag'>
-                        <svg>
-                        </svg>
-                            <h4>USD/JPY <br/> USD/JPY </h4>
-                            </div>
-                            <p className='rise'>+0.96</p>
-                            <div>
-                            <button className='sell'>Sell</button>
-                            <button className='buy'>Buy</button>
-                        </div>
-                    </div>
-                    </div>
-            </div>
-        </div>
-    );
+        <section>
+            van
+        </section>   
+    )
 }
 
 export default Works;
